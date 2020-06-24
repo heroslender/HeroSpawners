be5b33730e1cb28f02ae0cc5dfd86f6b0538a9ac
Bruno Martins <bruno.martins1998@hotmail.com>
2020-06-24 20:52:36 +0100

    update JH_StackMobs dependency

--- src/main/java/com/heroslender/herospawners/mobstacker/JhMobStacker.java
+++ src/main/java/com/heroslender/herospawners/mobstacker/JhMobStacker.java
@@ -1,13 +1,16 @@
 package com.heroslender.herospawners.mobstacker;
 
+import JH_StackMobs.API;
 import JH_StackMobs.Main;
 import com.heroslender.herospawners.models.ISpawner;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
-import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.entity.MagmaCube;
+import org.bukkit.entity.Slime;
+import ultils.IaRemover;
+import ultils.StackAll;
 
 public class JhMobStacker implements MobStackerSupport {
-    private static final String META_NAME = "JH_StackMobs";
 
     public JhMobStacker() {
         Bukkit.getLogger().info("[herospawners] Jh-MobStacker foi encontrado!");
@@ -15,27 +18,19 @@ public class JhMobStacker implements MobStackerSupport {
 
     @Override
     public boolean createOrAddStack(ISpawner spawner, Entity entity, int quantidade) {
-        for (Entity e : entity.getNearbyEntities(Main.getInstace().x, Main.getInstace().y, Main.getInstace().z)) {
-            if (e.getType() == entity.getType()) {
-                if (e.hasMetadata(META_NAME)) {
-                    quantidade = e.getMetadata(META_NAME).get(0).asInt() + quantidade;
+        API.setStackAmount(entity, quantidade);
+        if (!StackAll.stackNear(entity, false)) {
+            // New stack
+            if (entity instanceof MagmaCube) {
+                ((MagmaCube) entity).setSize(1);
+            } else if (entity instanceof Slime) {
+                ((Slime) entity).setSize(1);
+            }
 
-                    e.setMetadata(META_NAME, new FixedMetadataValue(Main.getInstace(), quantidade));
-                    e.setCustomName(JH_StackMobs.Main.getInstace().displayName
-                            .replace("{mob}", Main.getInstace().getTranslate(entity.getType()))
-                            .replace("{quantidade}", Integer.toString(quantidade)));
-                    e.setCustomNameVisible(true);
-                    return true;
-
-                }
+            if (Main.getInstace().removeIa()) {
+                IaRemover.remove(entity);
             }
         }
-
-        entity.setMetadata(META_NAME, new FixedMetadataValue(Main.getInstace(), quantidade));
-        entity.setCustomName(JH_StackMobs.Main.getInstace().displayName
-                .replace("{mob}", Main.getInstace().getTranslate(entity.getType()))
-                .replace("{quantidade}", Integer.toString(quantidade)));
-        entity.setCustomNameVisible(true);
         return false;
     }
 }
